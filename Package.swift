375b2021d00f90484dee064b08a03867df2b813e
Jacob Williams <ponyboy47@gmail.com>
2017-11-15 17:37:14 -0700

    Upgrade to swift-tools version 4.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "CSelect"
+    name: "CSelect",
+    products: [
+        .library(
+            name: "Cselect",
+            targets: ["Cselect"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Cselect",
+            dependencies: []),
+    ]
 )
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Expose some of the &lt;sys/select.h> file descriptor functions for swift
 
 Include the following line in your Package.swift as a dependency
 ```swift
-.Package(url: "https://github.com/Ponyboy47/CSelect.git", majorVersion: 1)
+.package(url: "https://github.com/Ponyboy47/CSelect.git", .upToNextMinor(from: "2.0.0"))
 ```
